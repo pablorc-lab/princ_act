f10b8d9b009522d9d998c31cf06e04570d8dfec2
pablorc-lab <prambladoc@gmail.com>
2024-11-25 00:56:45 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react'
 import Quiz from './components/quiz';
 import Home from './components/home';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/princ_act" element={<Home />} />
-      <Route path="/princ_act/quiz" element={<Quiz />} />
-      <Route path="/princ_act/practicar" element={<Quiz respuesta_inmediata={true} />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/princ_act" element={<Home/>} />
+        <Route path="/princ_act/quiz" element={<Quiz/>} />
+        <Route path="/princ_act/quiz" element={<Quiz/>} />
+      </Routes>
+    </Router>
   );
 }
 
