83e9fd5512cf659e216695f9d15a22723a2eb320
Pablo Ramblado Carrasco <prambladoc@gmail.com>
2024-12-29 18:10:53 +0100

    tips home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,23 +1,37 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Gastly from "../images/gastly.png"
+import Bombilla from "../images/bombilla.png"
+import { infoText } from './info_text';
 import "./home.css"
 
 export default function Home(){
-  return (
-    <div className='menu'>
-      <div className='gastly'>
-        <img src={Gastly} alt="gastly"/>
-      </div>
-      
-      <div className='botones'>
-        <Link to="/practicar">
-          <button>Practicar principios</button>
-        </Link>
+  const [tip_text, setTipText] = useState(null);
 
-        <Link to="/quiz">
-          <button>Hacer el Quiz</button>
-        </Link>        
+  return (
+    <>
+      <div className='menu'>
+        <div className='gastly'>
+          <img src={Gastly} alt="gastly"/>
+        </div>
+        
+        <div className='botones'>
+          <Link to="/practicar">
+            <button onMouseOver={()=>setTipText(infoText.practicar)} onMouseOut={()=> setTipText(null)}>Practicar principios</button>
+          </Link>
+
+          <Link to="/quiz">
+            <button onMouseOver={()=>setTipText(infoText.quiz)} onMouseOut={()=>setTipText(null)}>Hacer el Quiz</button>
+          </Link>        
+        </div>
       </div>
-    </div>
+
+      {tip_text && (
+        <article className='tip'>
+          <img src={Bombilla} alt="bombilla"/>
+          <p>{tip_text}</p>
+        </article>
+      )}
+    </>
   );
 }
---
+++ src/components/info_text.js
@@ -0,0 +1,6 @@
+export const infoText = {
+    practicar: 'Se mostrará cada principio activo, apareciendo la solución al responder, junto el resultado del usuario y la respuesta correcta para referencia',
+
+    quiz: 'El usuario ingresará el número de principios activos a resolver. Al finalizar, se muestra un resumen con los resultados obtenidos en base a las respuestas dadas',
+  };
+  
