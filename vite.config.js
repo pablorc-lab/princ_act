e04612b59fdea407e5dd37f570c14c69d5eacc31
pablorc-lab <prambladoc@gmail.com>
2025-02-03 16:24:16 +0100

    error deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/princ_act",
+  base: "https://pablorc-lab.github.io/princ_act",
   server: {
     port:3000,
     open: true
