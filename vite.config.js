ac7997a75510cb6da58df3c7c447658384ef44fc
pablorc-lab <prambladoc@gmail.com>
2025-02-03 16:14:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://pablorc-lab.github.io/princ_act",
+  base: "/princ_act",
   server: {
     port:3000,
     open: true
