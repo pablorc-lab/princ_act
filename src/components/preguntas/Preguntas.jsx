1a01f9afba0ec2e371832e165381ef61a3d868d4
pablorc-lab <prambladoc@gmail.com>
2025-02-07 17:36:51 +0100

    title

--- src/components/preguntas/Preguntas.jsx
+++ src/components/preguntas/Preguntas.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link,  } from 'react-router-dom'; 
+import { Link, } from 'react-router-dom';
 import './preguntas.css';
 import Atomo from "../../images/atomo.webp"
 import Continuar from "../../images/continuar.webp"
@@ -15,7 +15,7 @@ import Thumb from "../../images/thumb_up.svg"
 
 //	Este componente se encarga de mostrar los modos Quiz y Práctica
 //	`respuesta_inmediata` controla este cambio, donde será False o True respectivamente	
-export default function Quiz_preguntas({Preguntas, setIncorrectas, setShowResult, respuesta_inmediata = false, question_size=1, tipoDatos}) {
+export default function Quiz_preguntas({ Preguntas, setIncorrectas, setShowResult, respuesta_inmediata = false, question_size = 1, tipoDatos }) {
 	const [preguntasMezcladas, setPreguntasMezcladas] = useState([]);
 	const [index, setIndex] = useState(0);
 	const [principio_actual, setPrincipio] = useState(null);
@@ -26,14 +26,12 @@ export default function Quiz_preguntas({Preguntas, setIncorrectas, setShowResult
 
 	//*Desordenar las preguntas al cargar el componente
 	useEffect(() => {
-		document.title = (!respuesta_inmediata ? "Quiz " : "Practicar ") + `${tipoDatos}`;
-
 		const preguntasAleatorias = [...Preguntas].sort(() => Math.random() - 0.5);
 		setPreguntasMezcladas(preguntasAleatorias);
 		setPrincipio(preguntasAleatorias[0]);
 
-		if(!respuesta_inmediata){
-			setPreguntasMezcladas(preguntasAleatorias.slice(0, question_size)) 
+		if (!respuesta_inmediata) {
+			setPreguntasMezcladas(preguntasAleatorias.slice(0, question_size))
 		}
 	}, [Preguntas, question_size, respuesta_inmediata, tipoDatos]);
 
@@ -53,21 +51,21 @@ export default function Quiz_preguntas({Preguntas, setIncorrectas, setShowResult
 		// Normalizar el texto del usuario
 		const textoUser = quitarTildes(texto.toLowerCase().trim());
 		const textoUser_norm = quitarTildes(quitarEspacios(texto));
-		
+
 		// Si hay uso en ese principio comprobar
-		const regexUso = new RegExp('\\(([^)]+)\\)'); 
+		const regexUso = new RegExp('\\(([^)]+)\\)');
 		const hay_uso = textoUser_norm.match(regexUso);
 
-		if(uso && hay_uso){
+		if (uso && hay_uso) {
 			// Obtener el uso escrito por el usuario
 			return (regex.test(textoUser) || (tipo_norm === textoUser_norm)) && (uso_norm === hay_uso[0].trim());
 		}
-		
+
 		// En otro caso solo comprueba el principio
 		return regex.test(textoUser) || (tipo_norm === textoUser_norm);
 	}
 
-	
+
 	//* Cambiar principio
 	const Cambiar_principio = () => {
 		// Verificar si tiene uso añadido el principio actual
@@ -75,9 +73,9 @@ export default function Quiz_preguntas({Preguntas, setIncorrectas, setShowResult
 
 		if (!verificarRespuesta()) {
 			const nuevoValores = [
-        principio_actual.principio,
-        texto.charAt(0).toUpperCase() + texto.slice(1),
-        principio_actual.tipo.charAt(0).toUpperCase() + principio_actual.tipo.slice(1) + uso_actual,
+				principio_actual.principio,
+				texto.charAt(0).toUpperCase() + texto.slice(1),
+				principio_actual.tipo.charAt(0).toUpperCase() + principio_actual.tipo.slice(1) + uso_actual,
 			];
 
 			setResultados_act(nuevoValores);
@@ -98,7 +96,7 @@ export default function Quiz_preguntas({Preguntas, setIncorrectas, setShowResult
 				principio_actual.tipo.charAt(0).toUpperCase() + principio_actual.tipo.slice(1) + uso_actual
 			]);
 		}
-	
+
 		//* Si no hay mas preguntas se finaliza
 		if (index + 1 === preguntasMezcladas.length && (!respuesta_inmediata && !mostrar_solucion))
 			setShowResult(true);
@@ -108,7 +106,7 @@ export default function Quiz_preguntas({Preguntas, setIncorrectas, setShowResult
 		setTexto("");
 	};
 
-	
+
 	//*Función para manejar la pulsación de tecla Enter
 	const handleKeyPress = (event) => {
 		if (event.key === "Enter" && index < preguntasMezcladas.length && (texto || mostrar_solucion)) {
@@ -134,100 +132,104 @@ export default function Quiz_preguntas({Preguntas, setIncorrectas, setShowResult
 				updateResultados[1] = null
 				return updateResultados;
 			});
-			
+
 			setIncorrectas((prevIncorrectas) => {
 				const updatedIncorrectas = [...prevIncorrectas];
-				updatedIncorrectas.pop();  
-				return updatedIncorrectas; 
+				updatedIncorrectas.pop();
+				return updatedIncorrectas;
 			});
 		}
 	};
-	
+
 	return (
-		<div className={`App ${tipoDatos}`}>
-			<Link to="/">
+		<>
+			<title>{(!respuesta_inmediata ? "Quiz " : "Practicar ")} {tipoDatos}</title>
+			<div className={`App ${tipoDatos}`}>
+				<Link to="/">
+					<img
+						src={Casa}
+						alt="home logo"
+						style={{
+							position: 'absolute',
+							left: '20px',
+							top: '20px',
+							width: '90px',
+							cursor: 'pointer',
+							opacity: '0.5',
+						}}
+						onMouseOver={(e) => (e.currentTarget.style.opacity = '1')}
+						onMouseOut={(e) => (e.currentTarget.style.opacity = '0.5')}
+					/>
+				</Link>
+				<img className='Gengar' src={`${tipoDatos === "principios" ? Gengar : Funko_mandragora}`} alt="gengar" />
 				<img
-					src={Casa}
-					alt="home logo"
-					style={{
-						position: 'absolute',
-						left: '20px',
-						top: '20px',
-						width: '90px',
-						cursor: 'pointer',
-						opacity: '0.5',
-					}}
-					onMouseOver={(e) => (e.currentTarget.style.opacity = '1')}
-					onMouseOut={(e) => (e.currentTarget.style.opacity = '0.5')}
-				/>
-			</Link>
-			<img className='Gengar' src={`${tipoDatos === "principios" ? Gengar : Funko_mandragora}`} alt="gengar" />
-			<img 
-				className={`${tipoDatos === "principios" ? "puntos" : "enredadera"}`} 
-				src={`${tipoDatos === "principios" ? Puntos : Enredadera}`} 
-				alt="Puntos" 
+					className={`${tipoDatos === "principios" ? "puntos" : "enredadera"}`}
+					src={`${tipoDatos === "principios" ? Puntos : Enredadera}`}
+					alt="Puntos"
 				/>
 
-			<section className='Papel_container'>
-				{/*Solucion inmediata*/}
-				{respuesta_inmediata && mostrar_solucion && (
-					<div className='Papel_solt'>
-						<h2>{resultados_actuales[0]}</h2>
+				<section className='Papel_container'>
+					{/*Solucion inmediata*/}
+					{respuesta_inmediata && mostrar_solucion && (
+						<div className='Papel_solt'>
+							<h2>{resultados_actuales[0]}</h2>
 
-						<article className='solutions_container'>
-							<div className={resultados_actuales[1] ? 'incorrecta' : 'correcta'}>
-								<img 
-									src={resultados_actuales[1] ? Cross : Check} 
-									alt={resultados_actuales[1] ? 'cross' : 'check'} 
-								/>
-								<p>{resultados_actuales[1] || resultados_actuales[2]}</p>
-							</div>
-							<div className='correcta'>
-								<img src={Check} alt="check" />
-								<p>{resultados_actuales[2]}</p>
-							</div>
-						</article>
+							<article className='solutions_container'>
+								<div className={resultados_actuales[1] ? 'incorrecta' : 'correcta'}>
+									<img
+										src={resultados_actuales[1] ? Cross : Check}
+										alt={resultados_actuales[1] ? 'cross' : 'check'}
+									/>
+									<p>{resultados_actuales[1] || resultados_actuales[2]}</p>
+								</div>
+								<div className='correcta'>
+									<img src={Check} alt="check" />
+									<p>{resultados_actuales[2]}</p>
+								</div>
+							</article>
 
-						<div className="thumb" onMouseEnter={() => setResiveAnswer(!reviseAnswer)} onMouseLeave={() => setResiveAnswer(!reviseAnswer)}>
-							<img src={Thumb} alt='thunb' width="50px" onClick={handleResiveAnswer}/>
-							{reviseAnswer && <p>Si tu respuesta es correcta pulsa para corregir</p>}
+							<div className="thumb" onMouseEnter={() => setResiveAnswer(!reviseAnswer)} onMouseLeave={() => setResiveAnswer(!reviseAnswer)}>
+								<img src={Thumb} alt='thunb' width="50px" onClick={handleResiveAnswer} />
+								{reviseAnswer && <p>Si tu respuesta es correcta pulsa para corregir</p>}
+							</div>
 						</div>
-					</div>
-				)}
+					)}
 
-				{/*Papel*/}
-				{!mostrar_solucion && (
-					<section className={`Papel ${tipoDatos}`}>
-						<p className='contador'> 
-							{principio_actual ? `${index + 1} / ${preguntasMezcladas.length}` : '-/-'}
-						</p>
-						<h1>
-							{principio_actual ? principio_actual.principio : "..."}
-						</h1>
-					</section>
-				)}
-			</section>
+					{/*Papel*/}
+					{!mostrar_solucion && (
+						<section className={`Papel ${tipoDatos}`}>
+							<p className='contador'>
+								{principio_actual ? `${index + 1} / ${preguntasMezcladas.length}` : '-/-'}
+							</p>
+							<h1>
+								{principio_actual ? principio_actual.principio : "..."}
+							</h1>
+						</section>
+					)}
+				</section>
 
-			<section className={`Respuesta ${tipoDatos}`} style={{backgroundColor: mostrar_solucion && "rgba(195, 190, 237, 0.1)"}}>
-				<img id="Atomo_img" src={`${tipoDatos === "principios" ? Atomo : Planta}`} alt="atomo" style={{ opacity: mostrar_solucion ? 0.3 : 1 }}				/>
-				<input
-				 	style={{ opacity: mostrar_solucion ? 0 : 1 }}
-					type='text'
-					autoFocus
-					spellCheck="false"
-					placeholder='Escribe su uso...'
-					value={texto}
-					onChange={(e) => {setTexto(e.target.value);}}
-					onKeyDown={handleKeyPress}
-				/>
-				<img
-					id="Continuar_img"
-					src={Continuar}
-					alt="continuar"
-					onClick={() => handleKeyPress({key: 'Enter'})} 
-					style={{ cursor: (texto||mostrar_solucion) ? 'pointer' : 'not-allowed', opacity: (texto||mostrar_solucion) ? 1 : 0.3 }}
-				/>
-			</section>
-		</div>
+				<section className={`Respuesta ${tipoDatos}`} style={{ backgroundColor: mostrar_solucion && "rgba(195, 190, 237, 0.1)" }}>
+					<img id="Atomo_img" src={`${tipoDatos === "principios" ? Atomo : Planta}`} alt="atomo" style={{ opacity: mostrar_solucion ? 0.3 : 1 }} />
+					<input
+						style={{ opacity: mostrar_solucion ? 0 : 1 }}
+						type='text'
+						autoFocus
+						spellCheck="false"
+						placeholder='Escribe su uso...'
+						value={texto}
+						onChange={(e) => { setTexto(e.target.value); }}
+						onKeyDown={handleKeyPress}
+					/>
+					<img
+						id="Continuar_img"
+						src={Continuar}
+						alt="continuar"
+						onClick={() => handleKeyPress({ key: 'Enter' })}
+						style={{ cursor: (texto || mostrar_solucion) ? 'pointer' : 'not-allowed', opacity: (texto || mostrar_solucion) ? 1 : 0.3 }}
+					/>
+				</section>
+			</div>
+		</>
+
 	);
 }
--- src/components/resultados/resultados.jsx
+++ src/components/resultados/resultados.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'; 
+import React from 'react'
+import { Link } from 'react-router-dom';
 import "./resultados.css"
 import Dot from "../../images/dot.webp"
 import Cross from "../../images/cross.webp"
@@ -11,70 +11,69 @@ export default function Final_result({ tam_preguntas, incorrectas, handleReinici
 	const correctas = tam_preguntas - incorrectas.length;
 	const nota = ((correctas / tam_preguntas) * 10).toFixed(2);
 
-	useEffect(() => {
-		document.title = "Resultados obtenidos";
-	})
-
 	return (
-		
-		<div className="result">
-			<Link to="/">
-				<img
-					src={Casa}
-					alt="home logo"
-					style={{
-						position: 'absolute',
-						left: '20px',
-						top: '20px',
-						width: '90px',
-						cursor: 'pointer',
-						opacity: '0.5',
-						filter: 'invert(1)'
-					}}
-					onMouseOver={(e) => (e.currentTarget.style.opacity = '1')}
-					onMouseOut={(e) => (e.currentTarget.style.opacity = '0.5')}
-				/>
-			</Link>
+		<>
+			<title>Resultados obtenidos</title>
+			<div className="result">
+				<Link to="/">
+					<img
+						src={Casa}
+						alt="home logo"
+						style={{
+							position: 'absolute',
+							left: '20px',
+							top: '20px',
+							width: '90px',
+							cursor: 'pointer',
+							opacity: '0.5',
+							filter: 'invert(1)'
+						}}
+						onMouseOver={(e) => (e.currentTarget.style.opacity = '1')}
+						onMouseOut={(e) => (e.currentTarget.style.opacity = '0.5')}
+					/>
+				</Link>
 
-			<section className='nota_container'>
-				<div className='correctas'>
-					<h1>CORRECTAS</h1>
-					<h2>{correctas} / {tam_preguntas}</h2>
-				</div>
-				<div className='nota'>
-					<h1>NOTA</h1>
-					<h2>{nota}</h2>
-				</div>
-				<img src={Retry} onClick={handleReiniciar} />
-			</section>
+				<section className='nota_container'>
+					<div className='correctas'>
+						<h1>CORRECTAS</h1>
+						<h2>{correctas} / {tam_preguntas}</h2>
+					</div>
+					<div className='nota'>
+						<h1>NOTA</h1>
+						<h2>{nota}</h2>
+					</div>
+					<img src={Retry} onClick={handleReiniciar} />
+				</section>
 
-			<section className='incorrectas_container'>
-				<div className='incorrectas'>
-					<h1>INCORRECTAS</h1>
-					<h2>{incorrectas.length}</h2>
-				</div>
+				<section className='incorrectas_container'>
+					<div className='incorrectas'>
+						<h1>INCORRECTAS</h1>
+						<h2>{incorrectas.length}</h2>
+					</div>
 
-				<ul className='incorrectas_text'>
-					{incorrectas.map((respuesta, index) => (
-						<li key={index}>
-							<div className='principio'>
-								<img src={Dot} alt="circulo" />
-								<span>{respuesta.principio}</span>
-							</div>
+					<ul className='incorrectas_text'>
+						{incorrectas.map((respuesta, index) => (
+							<li key={index}>
+								<div className='principio'>
+									<img src={Dot} alt="circulo" />
+									<span>{respuesta.principio}</span>
+								</div>
 
-							<div className='incorrecta'>
-								<img src={Cross} alt="cross" />
-								<span>{respuesta.incorrecta}</span>
-							</div>
+								<div className='incorrecta'>
+									<img src={Cross} alt="cross" />
+									<span>{respuesta.incorrecta}</span>
+								</div>
 
-							<div className='correcta'>
-								<img src={Check} alt="check" />
-								<span>{respuesta.correcta}</span>
-							</div>
-						</li>
-					))}
-				</ul>
-			</section>
-		</div>
+								<div className='correcta'>
+									<img src={Check} alt="check" />
+									<span>{respuesta.correcta}</span>
+								</div>
+							</li>
+						))}
+					</ul>
+				</section>
+			</div>
+		</>
+
 	);
 }
